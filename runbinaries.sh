77f09da7fd7ab68a6ee2de597c82f7e76bcc189e
beaudoco <beaudoco@mail.gvsu.edu>
2022-10-22 15:08:18 -0400

    updating run file

--- runbinaries.sh
+++ runbinaries.sh
@@ -1,11 +1,11 @@
-/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=m5out_Puzzle/base-large --redirect-stdout --redirect-stderr ~/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=1 --binfile=~/cse530_a4/benchmarks/Puzzle
-cp ~/cse530_a4/benchmarks/Puzzle ~/cse530_a4/benchmarks/Puzzle-base-large
-/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=m5out_Puzzle/base-small --redirect-stdout --redirect-stderr ~/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=2 --binfile=~/cse530_a4/benchmarks/Puzzle
-cp ~/cse530_a4/benchmarks/Puzzle ~/cse530_a4/benchmarks/Puzzle-base-small
-/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=m5out_Puzzle/small-width-4 --redirect-stdout --redirect-stderr ~/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=3 --binfile=~/cse530_a4/benchmarks/Puzzle
-cp ~/cse530_a4/benchmarks/Puzzle ~/cse530_a4/benchmarks/Puzzle-small-width-4
+/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=m5out_Puzzle/base-large --redirect-stdout --redirect-stderr ~/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=1 --binfile=/home/other/CSE530-FA2022/cse530_a4/benchmarks/Puzzle
+# cp ~/cse530_a4/benchmarks/Puzzle ~/cse530_a4/benchmarks/Puzzle-base-large
+/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=m5out_Puzzle/base-small --redirect-stdout --redirect-stderr ~/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=2 --binfile=/home/other/CSE530-FA2022/cse530_a4/benchmarks/Puzzle
+# cp ~/cse530_a4/benchmarks/Puzzle ~/cse530_a4/benchmarks/Puzzle-base-small
+/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=m5out_Puzzle/small-width-4 --redirect-stdout --redirect-stderr ~/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=3 --binfile=/home/other/CSE530-FA2022/cse530_a4/benchmarks/Puzzle
+# cp ~/cse530_a4/benchmarks/Puzzle ~/cse530_a4/benchmarks/Puzzle-small-width-4
 /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=m5out_Puzzle/small-width-6 --redirect-stdout --redirect-stderr ~/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=4 --binfile=/home/other/CSE530-FA2022/cse530_a4/benchmarks/Puzzle
-cp ~/cse530_a4/benchmarks/Puzzle ~/cse530_a4/benchmarks/Puzzle-small-width-6
+# cp ~/cse530_a4/benchmarks/Puzzle ~/cse530_a4/benchmarks/Puzzle-small-width-6
 /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=m5out_Puzzle/small-width-8 --redirect-stdout --redirect-stderr ~/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=5 --binfile=/home/other/CSE530-FA2022/cse530_a4/benchmarks/Puzzle
 # cp ~/cse530_a4/benchmarks/Puzzle ~/cse530_a4/benchmarks/Puzzle-small-width-8
 /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=m5out_Puzzle/small-tournament --redirect-stdout --redirect-stderr ~/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=6 --binfile=/home/other/CSE530-FA2022/cse530_a4/benchmarks/Puzzle
